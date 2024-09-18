0ce29c37b2ad6dcdc7de443bbc57fd95ab86e0f1
Md Nizam Sapiee <78304538+iamdata999@users.noreply.github.com>
2024-09-19 03:08:24 +0800

    updated

---
+++ Create_Table/CREATE TABLE foodrecommendation (.sql
@@ -0,0 +1,8 @@
+CREATE TABLE foodrecommendation (
+    cuisine VARCHAR(255),
+    number_of_orders INT,
+    customer_id INT,
+    commission_rate DECIMAL(5, 2),
+    delivery_fee_min DECIMAL(5, 2),
+    delivery_fee_max DECIMAL(5, 2)
+);
